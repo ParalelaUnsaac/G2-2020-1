862b14ab2d79f124813c029e4c18b5a96d855fdb
AmaruVL <182940@unsaac.edu.pe>
2020-10-07 22:28:48 -0500

    Update Func1.cpp

--- ProyectoCodigo/AppMatrices/Func1.cpp
+++ ProyectoCodigo/AppMatrices/Func1.cpp
@@ -1,1 +1,25 @@
 //Transpuesta de una matriz
+int** Data::transMatrix(int** matrix)
+{
+	int** transMatr = new int*[sizeOfArrays];
+
+	for (int i = 0; i < sizeOfArrays; i++)
+	{
+		transMatr[i] = new int[sizeOfArrays];
+	}
+
+	for (int i = 0; i < sizeOfArrays; i++)
+	{
+		for (int j = 0; j < sizeOfArrays; j++)
+		{
+			transMatr[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < sizeOfArrays; i++) {
+		for (int j = 0; j < sizeOfArrays; j++) {
+			transMatr[i][j] = matrix[j][i];
+		}
+	}
+	return transMatr;
+}
