7da1d48291cd0d416cca61d6a3713ee278996cf7
Richard3971 <72510951+Richard3971@users.noreply.github.com>
2020-10-21 10:32:37 -0500

    Inclusion de una nueva funcion
    Se le incremento la funcion 4

--- ProyectoCodigo/AppMatrices/Main.cpp
+++ ProyectoCodigo/AppMatrices/Main.cpp
@@ -2,48 +2,37 @@
 #include <iostream>
 #include <ctime>
 
-#include "Metodos.h"
-using namespace std;
+#include "Func1.h"
+#include "Func2.h"
+#include "Func3.h"
+
 
 int main()
 {
 
-	int opcion;
-	printf("============================ Menu Principal ================================\n");
-	cout << "1-  Opcion 1\n";
-	cout << "2-  Opcion 2\n";
-	cout << "3-  Opcion 3\n";
-	cout << "4-  Opcion 4\n";
-	printf("Seleccione una de las opciones:");
-	cin >> opcion;
-	printf("=============================================================================\n");
 #pragma omp parallel sections num_threads(4)
 	{
 
-
-		switch (opcion)
-		{
-		case 1:
 #pragma omp section
 
-			
-			break;
-		case 2:
+		Func1();
+
+
 #pragma omp section
 
-			
-			break;
-		case 3:
+		Func2();
+
+
 #pragma omp section
 
-			
-			break;
-		case 4:
+		Func3();
+
+
 #pragma omp section
 
-			break;
+		Func4();
 
-		}
 	}
+
 	return 0;
 }
